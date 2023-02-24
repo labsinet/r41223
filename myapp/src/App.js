4527d36b2e4c452feea40c4025e972df10b1dd9b
MentorChITA <mentorchita@gmail.com>
2023-02-24 15:43:24 +0200

    task 3

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import Header from './components/shared/Header/Header';
+import ProductList from './components/ProductList/ProductList';
 import './App.css';
 
 function App() {
+
+  const [products, setProducts] = useState([
+    {id: 1, title: 'Product 1', price: 899},
+    {id: 2, title: 'Product 2', price: 982},
+    {id: 3, title: 'Product 3', price: 322},
+    {id: 4, title: 'Product 4', price: 763},
+    {id: 5, title: 'Product 5', price: 389}
+  ]);
+
+    const deleteProduct = (productId) => {
+      const newProducts = products.filter(product => product.id !== productId);
+      setProducts(newProducts);
+    }
+
+
   return (
     <div className="App">
      <Header/> 
-     <h1>My first project</h1>
+      <ProductList products={products } deleteProduct={ deleteProduct } />
+
+
     </div>
   );
 }
---
+++ myapp/src/components/ProductList/ProductList.jsx
@@ -0,0 +1,19 @@
+import "./ProductList.scss"
+
+const ProductList= ({products, deleteProduct}) => {
+    return (
+        <div>
+           <ul>
+           {products.map((product) => (
+              <li key= {product.id}>  {product.title} - {product.price}
+              <button onClick={ () => deleteProduct(product.id )}> Delete</button>
+              </li>
+                 )
+
+               )}
+           </ul>
+        </div>
+    )
+}
+
+export default ProductList;
